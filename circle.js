462b29acaa438892ea0d086e47f6d3a00f9de3da
Luis Mcmillan <mcmillan.luis@googlemail.com>
2024-08-21 20:46:12 +0200

    version 20240821

--- circle.js
+++ circle.js
@@ -1,45 +1,51 @@
 export default class circle {
-    
-    constructor(id,category,name,x, y,target_x,target_y,size, content) {
+    constructor(id,category,is_boss,name,x, y,target_x,target_y,size, content) {
       this.id = id;
       this.x = x;
       this.y = y;
       this.target_x = target_x;
       this.target_y = target_y;
-      this.vx = 2;
-      this.vy = 2;
+      this.vx = 0.1;
+      this.vy = 0.1;
       this.radius = size;
       this.content = content;
       this.dragging = false;
       this.name = name;
       this.category = category;
+      if(is_boss === "true"){
+        this.is_boss = true;
+      }else {
+        this.is_boss = false;
+      }
       this.color = this.set_color();
       this.parent_links = [];
       this.child_links = [];
       this.hovered = false;
       this.attraction = 800/document.getElementById("gravity").value;
       this.rejection = document.getElementById("rejection")/100*this.attraction;
+      this.in_position = false;
+      
     }
 
     draw() {
         if(this.hovered){
-            this.draw_me_once("black");
-            this.show_text()
+            this.draw_me_once("white");
             for(let i = 0; i< this.child_links.length;i++){
-                this.child_links[i].draw_me_once("black");
-                this.child_links[i].show_text();
+                this.child_links[i].draw_me_once("white");
+                //this.child_links[i].show_text();
                 this.draw_line_to(this.child_links[i]);
             }
             for(let i = 0; i< this.parent_links.length;i++){
-                this.parent_links[i].draw_me_once("black");
-                this.parent_links[i].show_text();
+                this.parent_links[i].draw_me_once("white");
+                //this.parent_links[i].show_text();
                 this.draw_line_to(this.parent_links[i]);
             }
-
+            
         }else{
             
             this.draw_me_normal();
         }
+        //this.show_text();
       }
 
       draw_line_to(ball) {
@@ -47,8 +53,8 @@ export default class circle {
         const ctx = canvas.getContext("2d");
         const old_strokestyle = ctx.strokeStyle;
         const old_linewidth = ctx.lineWidth;
-        ctx.strokeStyle = 'black'; // Farbe der Linie
-        ctx.lineWidth = 2;   
+        ctx.strokeStyle = "white"; // Farbe der Linie
+        ctx.lineWidth = 3;   
         ctx.moveTo(this.x, this.y);
         ctx.lineTo(ball.x, ball.y);
         ctx.stroke();
@@ -56,44 +62,39 @@ export default class circle {
         ctx.lineWidth = old_linewidth;  
     }
     
-    follow(parent){
-      var distance = Math.sqrt((parent.x-this.x)**2 + (parent.y-this.y)**2);
-      if (!this.dragging && distance > this.attraction) {
-          this.x = this.x - this.vx*((this.x-parent.x)/(Math.abs(this.x-parent.x) + Math.abs(this.y-parent.y)));
-          this.y = this.y - this.vy*((this.y-parent.y)/(Math.abs(this.x-parent.x) + Math.abs(this.y-parent.y)));
-      } else if (!this.dragging && distance < this.rejection ){
-          this.x = this.x + this.vx*((this.x-parent.x)/(Math.abs(this.x-parent.x) + Math.abs(this.y-parent.y)));
-          this.y = this.y + this.vy*((this.y-parent.y)/(Math.abs(this.x-parent.x) + Math.abs(this.y-parent.y)));
-      }
-    }
 
     follow(){
+
         var distance = Math.sqrt((this.target_x-this.x)**2 + (this.target_y-this.y)**2);
-        if (!this.dragging && distance > 1.0) {
+        if (!this.dragging && distance > 8.0) {
             this.x = this.x - this.vx*((this.x-this.target_x)/(Math.abs(this.x-this.target_x) + Math.abs(this.y-this.target_y)));
             this.y = this.y - this.vy*((this.y-this.target_y)/(Math.abs(this.x-this.target_x) + Math.abs(this.y-this.target_y)));
-        } else if (!this.dragging && distance < 1.0){
+        } else if (!this.dragging && distance < 8.0){
             this.x = this.target_x;
             this.y = this.target_y;
+            this.in_position = true;
+        }
+        if (this.vx < 8){
+          this.vx += 0.05;
+          this.vy += 0.05;
         }
     }
 
-    keep_distance_to(neighbor){
-      var distance = Math.sqrt((neighbor.x-this.x)**2 + (neighbor.y-this.y)**2);
-      if (distance < 50) {
-          this.x = this.x + this.vx*((this.x-neighbor.x)/(Math.abs(this.x-neighbor.x) + Math.abs(this.y-neighbor.y)))
-          this.y = this.y + this.vy*((this.y-neighbor.y)/(Math.abs(this.x-neighbor.x) + Math.abs(this.y-neighbor.y)))
-      }
-    }
+    
 
     show_text(){
-        const canvas = document.getElementById("canvas");
-        const ctx = canvas.getContext("2d");
-        const old_color = ctx.fillStyle;
-        ctx.fillStyle = "black";
-        ctx.fillText(this.name, this.x, this.y - 5);
-        ctx.fill();
-        ctx.fillStyle = old_color;
+      if(this.hovered || this.dragging || this.is_boss){
+          console.log(this.hovered +" " +this.dragging+" "+ this.is_boss);
+          const canvas = document.getElementById("canvas");
+          const ctx = canvas.getContext("2d");
+          const old_color = ctx.fillStyle;
+          ctx.font = '20px Arial';
+          ctx.textAlign = 'center';
+          ctx.fillStyle = "black";
+          ctx.fillText(this.name, this.x, this.y - this.radius);
+          ctx.stroke();
+          ctx.fillStyle = old_color;
+      }
     }
 
     draw_me_normal(){
@@ -172,11 +173,8 @@ export default class circle {
                 return "aqua";
                 // Code, der ausgeführt wird, wenn expression === value1
               break;
-            case "Shell":
-                return "black";
-                // Code, der ausgeführt wird, wenn expression === value1
-              break;
-            case "Oracle Java":
+            
+            case "Java":
                 return "red";
               break;
             case "HTML":
@@ -205,6 +203,10 @@ export default class circle {
               break;
             case "SQL":
                 return "DarkMagenta";
+                // Code, der ausgeführt wird, wenn expression === value1
+              break;
+              case "Shell":
+                return "black";
                 // Code, der ausgeführt wird, wenn expression === value1
               break;
             // Weitere cases nach Bedarf
--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 import circle from "./circle.js";
-const jsoncontent = await loadJson();
-
 const canvas = document.getElementById("canvas");
 var circle_size = document.getElementById("circle_size").value;
 const ctx = canvas.getContext("2d");
@@ -10,62 +8,89 @@ ctx.strokeStyle = "gray";
 let raf;
 let general_dragged = false;
 let general_hovered = false;
+let all_loaded = false;
 let running = false;
+let lines_disappear_animation_done = false;
+let starting_animation_done = false;
+let animiation_color = 65;
+let matrix;
 const balls = [];
 const map = new Map();
-//Creates all circles
-var i = 0;
-for (const element of jsoncontent) {
-  map.set(element.name, element.id);
-  balls.push(
-    new circle(
-      element.id,
-      element.category,
-      element.name,
-      ctx_width / 2 +
-        ctx_width * 0.45 * Math.sin((i / jsoncontent.length) * 2 * Math.PI),
-      ctx_height / 2 +
-        ctx_width * 0.45 * Math.cos((i / jsoncontent.length) * 2 * Math.PI),
-      element.x_pos,
-      element.y_pos,
-      circle_size,
-      element.content
-    )
+main();
+
+
+
+
+async function main(){
+  const jsoncontent = await loadJson();
+  //Creates all circles
+  var i = 0;
+  for (const element of jsoncontent) {
+    map.set(element.name, element.id);
+    balls.push(
+      new circle(
+        element.id,
+        element.category,
+        element.is_boss,
+        element.name,
+        ctx_width / 2 +
+          ctx_width * 0.45 * Math.sin((i / jsoncontent.length) * 2 * Math.PI),
+        ctx_height / 2 +
+          ctx_width * 0.45 * Math.cos((i / jsoncontent.length) * 2 * Math.PI),
+        element.x_pos,
+        element.y_pos,
+        circle_size,
+        element.content
+      )
+    );
+    i++;
+  }
+  
+
+  //Creates all connections
+  for (const element of jsoncontent) {
+    const children_list = [];
+    const parent_list = [];
+    for(const child of element.children){
+      children_list.push(balls[map.get(child)]);
+    }
+    for(const parent of element.parents){
+      parent_list.push(balls[map.get(parent)]);
+    }
+    balls[element.id].child_links= children_list;
+    balls[element.id].parent_links= parent_list;
+  }
+
+  //Creates connectivity matrix
+  matrix = Array.from({ length: balls.length }, () =>
+    Array(balls.length).fill(0)
   );
-  i++;
-}
-//Creates all connections
-for (const element of jsoncontent) {
-  const children_list = [];
-  const parent_list = [];
-  for(const child of element.children){
-    children_list.push(balls[map.get(child)]);
+  for(const ball of balls){
+    let children_links = ball.get_child_links();
+    for (let i = 0; i < children_links.length; i++) {
+      matrix[ball.id][map.get(children_links[i].name)] = 1;
+    } 
   }
-  for(const parent of element.parents){
-    parent_list.push(balls[map.get(parent)]);
+  
+  draw_all_lines(`rgb(${animiation_color} ${animiation_color} ${animiation_color}`);
+  /*
+  
+  */
+  
+  all_loaded = true;
+  for (const element of balls) {
+    element.change_circle_size();
+    element.draw();
   }
-  balls[element.id].child_links= children_list;
-  balls[element.id].parent_links= parent_list;
-}
 
-//Creates connectivity matrix
-let matrix = Array.from({ length: balls.length }, () =>
-  Array(balls.length).fill(0)
-);
-for(const ball of balls){
-  let children_links = ball.get_child_links();
-  for (let i = 0; i < children_links.length; i++) {
-    matrix[ball.id][map.get(children_links[i].name)] = 1;
-  } 
+  
+
 }
 
 
-for (const element of balls) {
-  element.change_circle_size();
-  element.draw();
-}
 
-draw_all_lines(matrix);
+
+
 
 
 function clear() {
@@ -73,35 +98,82 @@ function clear() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function draw_line_between(ball1, ball2) {
+async function draw_line_between(ball1, ball2,color) {
+  const old_strokestyle = ctx.strokeStyle;
+  ctx.strokeStyle = color; // Farbe der Linie 
   ctx.moveTo(ball1.x, ball1.y);
   ctx.lineTo(ball2.x, ball2.y);
   ctx.stroke();
+  ctx.strokeStyle = old_strokestyle;
 }
 
-function draw() {
-  clear();
-  draw_all_lines(matrix);
+async function draw() {
+  console.log("draw");
+  if (all_loaded){
+    clear();
+    
+    draw_all_lines(`rgb(${animiation_color} ${animiation_color} ${animiation_color}`);
 
+    for (let i = 0; i < matrix.length; i++) {
+      balls[i].follow();
+    }
 
-  for (let i = 0; i < matrix.length; i++) {
-    balls[i].follow();
-    /*
-    for (let j = 0; j < matrix.length; j++) {
-      if (matrix[j][i] == 1) {
-        balls[i].follow(balls[j]);
+    for (const element of balls) {
+      element.change_circle_gravity();
+      element.change_circle_size();
+      element.draw();
+      //draw_me(element);
+    }
+  raf = window.requestAnimationFrame(draw);
+  }
+  
+}
+
+async function animation() {
+  if (all_loaded){
+    if(general_hovered || !starting_animation_done){
+      clear();
+    }
+    
+    if ((animiation_color < 215 && !lines_disappear_animation_done) ){
+      animiation_color +=10;
+      draw_all_lines(`rgb(${animiation_color} ${animiation_color} ${animiation_color}`);
+    }
+    if ((lines_disappear_animation_done && animiation_color > 65) ){
+      if (animiation_color > 65){
+        animiation_color -=10;
+      }
+      draw_all_lines(`rgb(${animiation_color} ${animiation_color} ${animiation_color}`);
+    }
+    for (let i = 0; i < matrix.length; i++) {
+      if (animiation_color == 215){
+        balls[i].follow();
       }
     }
-    */
-  }
-
-  for (const element of balls) {
-    element.change_circle_gravity();
-    element.change_circle_size();
-    element.draw();
-    //draw_me(element);
-  }
-  raf = window.requestAnimationFrame(draw);
+    if (animiation_color <= 65 && lines_disappear_animation_done){
+      if (general_hovered){
+        draw_all_lines(`rgb(${animiation_color} ${animiation_color} ${animiation_color}`);
+      }
+    }
+    let found_out_of_position = false;
+    for (const element of balls) {
+      element.change_circle_gravity();
+      element.change_circle_size();
+      element.draw();
+      
+      if (!element.in_position){
+        found_out_of_position = true;
+      }
+    }
+    for (const element of balls) {
+      element.show_text();
+    }
+    found_out_of_position ? lines_disappear_animation_done = false : lines_disappear_animation_done = true;
+    if (animiation_color <= 65 && lines_disappear_animation_done){
+      starting_animation_done = true;
+    }
+    raf = window.requestAnimationFrame(animation);
+  }  
 }
 
 async function loadJson() {
@@ -115,10 +187,15 @@ async function loadJson() {
 }
 
 canvas.addEventListener("mousemove", (e) => {
-  if (general_hovered) {
-    clear();
-    draw_all_lines(matrix);
+  if (all_loaded && !running && !starting_animation_done){
+    raf = window.requestAnimationFrame(animation);
+    running = true;
   }
+  if (all_loaded && general_hovered) {
+    clear();
+    draw_all_lines(`rgb(${animiation_color} ${animiation_color} ${animiation_color}`);
+  }
+  
   const rect = canvas.getBoundingClientRect();
   const scaleX = canvas.width / rect.width;
   const scaleY = canvas.height / rect.height;
@@ -144,14 +221,14 @@ canvas.addEventListener("mousemove", (e) => {
 
 canvas.addEventListener("click", (e) => {
   if (!running) {
-    raf = window.requestAnimationFrame(draw);
+    raf = window.requestAnimationFrame(animation);
     running = true;
   }
 });
 
 window.addEventListener("scroll", (e) => {
   if (!running) {
-    raf = window.requestAnimationFrame(draw);
+    raf = window.requestAnimationFrame(animation);
     running = true;
   }
 });
@@ -159,17 +236,23 @@ window.addEventListener("scroll", (e) => {
 document.getElementById("myModal"),
   addEventListener("click", (e) => {
     if (!running) {
-      raf = window.requestAnimationFrame(draw);
+      raf = window.requestAnimationFrame(animation);
       running = true;
     }
   });
 
 canvas.addEventListener("mouseout", (e) => {
+  if (!running) {
+    raf = window.requestAnimationFrame(animation);
+    running = true;
+  }
+  /*
   for (const element of balls) {
     element.dragging = false;
   }
   window.cancelAnimationFrame(raf);
   running = false;
+  */
 });
 
 canvas.addEventListener("mousedown", (e) => {
@@ -185,6 +268,8 @@ canvas.addEventListener("mousedown", (e) => {
       if (element.isPointInside(mouse_x, mouse_y)) {
         element.dragging = true;
         found_dragged = true;
+      }else{
+        element.dragging = false;
       }
     }
   }
@@ -192,11 +277,14 @@ canvas.addEventListener("mousedown", (e) => {
   found_dragged ? general_dragged = true : general_dragged = false;
 });
 
-function draw_all_lines(matrix) {
+
+
+async function draw_all_lines(color) {
+  
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix.length; j++) {
       if (matrix[i][j] == 1) {
-        draw_line_between(balls[i], balls[j]);
+        draw_line_between(balls[i], balls[j],color);//
       }
     }
   }
